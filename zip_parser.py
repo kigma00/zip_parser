8466f159e76edf15db75943e177f7ac18f4ff9f9
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2024-10-19 21:22:33 +0900

    Create zip_parser.py

---
+++ zip_parser.py
@@ -0,0 +1,103 @@
+#>> python .\zip_parser.py .\sample2.zip
+
+# End of central directory record
+File signature (Magic Number): 50 4B 05 06 
+Disk Start Number: 0
+Disk # w/cd: 0
+Disk Entry: 3
+Total Entry: 3
+Size of Central Directory: 322
+Central Header Offset: 3633193
+Comment Length: None
+
+----------------------------------------------------------
+
+# Central Directory File header
+
+#0. Central Directory File header
+File signature (Magic Number): 50 4B 01 02 
+Version made by: 20
+Version needed to extract (minimum): 20
+Flags: 0
+Compression method: 8
+Moditime/Modidate: 21976/22619
+CRC-32 CheckSum: 954847417
+Compressed Size/Uncompressed Size: 487394/487878
+File Name Length/Extra Field Length: 22/36
+File Comment Length: 0
+Disk Start Number: 0
+Internal Attribute: 0
+External Attribute: 536870912
+Local Header: 0
+File Name: korea-7366036_1280.jpg
+
+#1. Central Directory File header
+File signature (Magic Number): 50 4B 01 02 
+Version made by: 20
+Version needed to extract (minimum): 20
+Flags: 0
+Compression method: 8
+Moditime/Modidate: 19858/22619
+CRC-32 CheckSum: 3517024303
+Compressed Size/Uncompressed Size: 3125205/487878
+File Name Length/Extra Field Length: 8/55
+File Comment Length: 0
+Disk Start Number: 0
+Internal Attribute: 0
+External Attribute: 536870912
+Local Header: 376440576
+File Name: 컬러.png
+
+#2. Central Directory File header
+File signature (Magic Number): 50 4B 01 02 
+Version made by: 20
+Version needed to extract (minimum): 20
+Flags: 0
+Compression method: 8
+Moditime/Modidate: 29461/22619
+CRC-32 CheckSum: 540923877
+Compressed Size/Uncompressed Size: 20428/487878
+File Name Length/Extra Field Length: 8/55
+File Comment Length: 0
+Disk Start Number: 0
+Internal Attribute: 0
+External Attribute: 536870912
+Local Header: 606091008
+File Name: 흑백.png
+
+----------------------------------------------------------   
+
+# Local File Header
+
+#0. Local File Header
+File signature (Magic Number): 50 4B 03 04
+Version needed to extract: 20
+Flags: 0
+Compression method: 8
+Moditime/Modidate: 21976/22619
+CRC-32 CheckSum: 954847417
+Compressed Size/Uncompressed Size: 487394/487878
+File Name Length/Extra Field Length: 22/0
+File Name: korea-7366036_1280.jpg
+
+#1. Local File Header
+File signature (Magic Number): 50 4B 03 04
+Version needed to extract: 20
+Flags: 0
+Compression method: 8
+Moditime/Modidate: 19858/22619
+CRC-32 CheckSum: 3517024303
+Compressed Size/Uncompressed Size: 3125205/3124201
+File Name Length/Extra Field Length: 8/19
+File Name: 컬러.png
+
+#2. Local File Header
+File signature (Magic Number): 50 4B 03 04
+Version needed to extract: 20
+Flags: 0
+Compression method: 8
+Moditime/Modidate: 29461/22619
+CRC-32 CheckSum: 540923877
+Compressed Size/Uncompressed Size: 20428/20418
+File Name Length/Extra Field Length: 8/19
+File Name: 흑백.png
